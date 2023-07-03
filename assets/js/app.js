dcaaa043ae57adfed3abb9dfc2e5cce62703b9fd
Paulina-Ensastiga <celypau@gmail.com>
2023-07-03 17:45:15 -0600

    Activida revisando el código PAU

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const usersEndpoint = `${baseEndpoint}/users`; /*Se renombraron las constantes*/
+const $name = document.querySelector('name');
+const $blog = document.querySelector('blog'); /* Se corrigieron*/
+const $location = document.querySelector('location');
 
 function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  $name.textContent = 'cargando...';
+  const response = fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $name.textContent = '${data.name}';
+  $blog.textContent = '${data.blog}';
+  $location.textContent = '${data.location}';
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  name.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
